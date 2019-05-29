876743bf3e9daf0617b652a2fe600fe929717c6f
Alexia E. Smith <washuu@gmail.com>
2019-05-29 15:54:41 -0500

    Add icon and category functions.

--- includes/notification/Notification.php
+++ includes/notification/Notification.php
@@ -1,6 +1,91 @@
 <?php
-// Function ideas:
-// 1.) getIcon() - Returns appropriate icon for the notification.  Cascades down through getNotificationIcon(), getSubcategoryIcon(), and getCategoryIcon() as needed.
-// 2.) getNotificationIcon() - Get notification icon from $wgReverbIcons.
-// 3.) getSubcategoryIcon() - Get subcategory icon from $wgReverbIcons.
-// 4.) getCategoryIcon() - Get category icon from $wgReverbIcons.
+use Hydrawiki\Reverb\Client\V1\ClientFactory;
+use MediaWikiServices;
+
+class Notification {
+	/**
+	 * Data container.
+	 *
+	 * @var array
+	 */
+	private $data = [];
+
+	/**
+	 * Main Constructor
+	 *
+	 * @access	public
+	 * @return	void
+	 */
+	public function __construct() {
+
+	}
+
+	/**
+	 * Get the type for this notification.
+	 *
+	 * @access	public
+	 * @return	string	Notification Type
+	 */
+	public function getType(): string {
+		return $this->data['type'];
+	}
+
+	/**
+	 * Get the category for this notification.
+	 *
+	 * @access	public
+	 * @return	string	Category
+	 */
+	public function getCategory(): string {
+		return substr($this->getType(), 0, strpos($this->getType(), '-'));
+	}
+
+	/**
+	 * Get the subcategory for this notification.
+	 *
+	 * @access	public
+	 * @return	string	Subcategory
+	 */
+	public function getSubcategory(): string {
+		return substr($this->getType(), 0, strpos($this->getType(), '-', strpos($this->getType()) + 1));
+	}
+
+	/**
+	 * Return the URL for the notification icon.
+	 *
+	 * @access	public
+	 * @return	string|null URL or null if missing.
+	 */
+	public function getNotificationIcon(): ?string {
+		$mainConfig = MediaWikiServices::::getInstance()->getMainConfig();
+		$reverbIcons = $config->get('ReverbIcons');
+
+		return $reverbIcons['notification'][$this->getType()] ?? null;
+	}
+
+	/**
+	 * Return the URL for the category icon.
+	 *
+	 * @access	public
+	 * @return	string|null URL or null if missing.
+	 */
+	public function getCategoryIcon(): ?string {
+		$mainConfig = MediaWikiServices::::getInstance()->getMainConfig();
+		$reverbIcons = $config->get('ReverbIcons');
+
+		return $reverbIcons['category'][$this->getCategory()] ?? null;
+	}
+
+	/**
+	 * Return the URL for the subcategory icon.
+	 *
+	 * @access	public
+	 * @return	string|null URL or null if missing.
+	 */
+	public function getSubcategoryIcon(): ?string {
+		$mainConfig = MediaWikiServices::::getInstance()->getMainConfig();
+		$reverbIcons = $config->get('ReverbIcons');
+
+		return $reverbIcons['subcategory'][$this->getSubcategory()] ?? null;
+	}
+}
