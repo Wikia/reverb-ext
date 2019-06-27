512e8b7d5d0dd31eba3c6e9060a1ef42e3d6d5de
Alexia <washuu@gmail.com>
2019-06-27 20:21:15 +0000

    Email template.

--- includes/notification/NotificationEmail.php
+++ includes/notification/NotificationEmail.php
@@ -13,7 +13,10 @@ declare(strict_types=1);
 namespace Reverb\Notification;
 
 use Hydrawiki\Reverb\Client\V1\Resources\Notification as NotificationResource;
+use MediaWiki\MediaWikiServices;
 use Reverb\Traits\NotificationListTrait;
+use SpecialPage;
+use User;
 
 class NotificationEmail {
 	use NotificationListTrait;
@@ -76,17 +79,19 @@ class NotificationEmail {
 
 		$notification = new Notification($resource);
 
-		$header = $notification->getHeader(true);
-		$body = [
-			'text' => $header,
-			'html' => $this->getWrappedBody($header)
-		];
-
 		$targets = $this->getTargets();
 		$success = 0;
 		foreach ($targets as $user) {
 			if ($this->shouldNotify($user, $attributes['type'], 'email')) {
-				$status = $user->sendMail(strip_tags((string)$header), $body);
+				$notification->setUser($user);
+				$header = (string)$notification->getHeader(true);
+				$htmlBody = $this->getWrappedBody($notification, $user);
+				$body = [
+					'text' => $htmlBody,
+					'html' => $htmlBody
+				];
+
+				$status = $user->sendMail(strip_tags($header), $body);
 				if ($status->isGood()) {
 					$success++;
 				}
@@ -104,11 +109,48 @@ class NotificationEmail {
 	/**
 	 * Get the body of the email wrapped in header and footer.
 	 *
-	 * @param string $body The message body.
+	 * @param Notification $notification The notification to present.
+	 * @param User         $user         User context for language selection.
 	 *
 	 * @return string
 	 */
-	private function getWrappedBody($body): string {
-		return (string)$body;
+	private function getWrappedBody(Notification $notification, User $user): string {
+		$twig = MediaWikiServices::getInstance()->getService('TwiggyService');
+		$template = $twig->load('@Reverb/notification_email.twig');
+
+		$config = MediaWikiServices::getInstance()->getMainConfig();
+		$wgCanonicalServer = $config->get("CanonicalServer");
+
+		$wrapped = $template->render(
+			[
+				'wgCanonicalServer' => $wgCanonicalServer,
+				'header' => (string)$notification->getHeader(true),
+				'user_note' => $notification->getUserNote(),
+				'icon' => $notification->getNotificationIcon(),
+				'action' => $notification->getCanonicalUrl(),
+				'action_description' => wfMessage(
+					'reverb-email-action-description'
+				)->inLanguage($user->getOption('language')),
+				'footer' => $this->getFooter($user)
+			]
+		);
+		return (string)$wrapped;
+	}
+
+	/**
+	 * Get the body of the email wrapped in header and footer.
+	 *
+	 * @param User $user User context for language selection.
+	 *
+	 * @return string Assembled HTML
+	 */
+	private function getFooter(User $user): string {
+		$preferencesTitle = SpecialPage::getTitleFor('Preferences', false, 'mw-prefsection-reverb');
+		$footer = wfMessage(
+			'reverb-email-preferences-footer',
+			$preferencesTitle->getFullURL()
+		)->inLanguage($user->getOption('language'))->text();
+
+		return $footer;
 	}
 }
