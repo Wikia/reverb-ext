a17dd4d9c9b22bcbead806b01b07fbbc1a97acee
Samuel Hilson <samrhilson@gmail.com>
2019-08-07 13:15:22 -0500

    Add i18n alias file for translating special pages

---
+++ i18n/Reverb.i18n.alias.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Reverb
+ * i18n Alias File
+ *
+ * @package Reverb
+ * @author  Samuel Hilson
+ * @license GPL-2.0-or-later
+ **/
+
+$specialPageAliases = [];
+
+/**
+ * English
+ */
+$specialPageAliases['en'] = [
+	'Notifications' => ['Notifications'],
+];
+
+/**
+ * Polish
+ */
+$specialPageAliases['pl'] = [
+	'Notifications' => ['Powiadomienia'],
+];
