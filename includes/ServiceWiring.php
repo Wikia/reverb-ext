b29aad409fe4769e60e0c01ba1f283d411da0822
Máté Szabó <mszabo@fandom.com>
2021-05-14 16:02:49 +0200

    PLATFORM-5939 | Support calling Reverb through a local UNIX socket

--- includes/ServiceWiring.php
+++ includes/ServiceWiring.php
@@ -10,6 +10,7 @@
 
 declare(strict_types=1);
 
+use Http\Adapter\Guzzle6\Client;
 use Http\Discovery\HttpClientDiscovery;
 use Http\Discovery\MessageFactoryDiscovery;
 use Hydrawiki\Reverb\Client\V1\ClientFactory;
@@ -20,6 +21,23 @@ return [
 		$mainConfig = $services->getMainConfig();
 		$endPoint = $mainConfig->get('ReverbApiEndPoint');
 		$apiKey = $mainConfig->get('ReverbApiKey');
+		$envoySocketPath = $mainConfig->get('ReverbEnvoySocketPath');
+
+		// PLATFORM-5939: support calling Reverb through a local UDS
+		if ( $envoySocketPath ) {
+			$httpClient = Client::createWithConfig([
+				'curl' => [
+					CURLOPT_UNIX_SOCKET_PATH => $envoySocketPath,
+				],
+			]);
+
+			return (new ClientFactory)->make(
+				$httpClient,
+				MessageFactoryDiscovery::find(),
+				$endPoint,
+				$apiKey
+			);
+		}
 
 		return (new ClientFactory)->make(
 			HttpClientDiscovery::find(),
