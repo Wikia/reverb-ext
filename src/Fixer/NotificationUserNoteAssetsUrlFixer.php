c423532efd9668c8d27b0aa22c5d9c077df79adc
Paweł Świszcz <pswiszcz@fandom.com>
2023-05-15 13:16:34 +0200

    UXFCP-3645 (fix): Removed wrong assets url from notification user_note
    The assets should not have /extensions-ucp/v2 in url. It should be replaced with /extensions-ucp/mw139/

--- src/Fixer/NotificationUserNoteAssetsUrlFixer.php
+++ src/Fixer/NotificationUserNoteAssetsUrlFixer.php
@@ -12,6 +12,9 @@ final class NotificationUserNoteAssetsUrlFixer {
 		if ( !isset( $notification['user_note'] ) ) {
 			return $notification;
 		}
+
+		$notification['user_note'] = $this->replaceNotificationImgAssetUrl($notification['user_note']);
+
 		// replace relative path with full urls, so notifications displayed outside of wiki context
 		// (i.e. fandom.com page) work well
 		$notification['user_note'] = preg_replace(
@@ -21,5 +24,24 @@ final class NotificationUserNoteAssetsUrlFixer {
 		);
 
 		return $notification;
+	}
+
+	private function replaceNotificationImgAssetUrl($userNote): array|string|null
+	{
+		$regex = '/(<img\s+[^>]*?\bsrc=["\'].*?)((?!.*\/extensions-ucp\/mw139\/.*).*|^(?=.*\/extensions-ucp\/v2\/.*$).*|^(?=.*\/extensions-ucp\/[^\/]+\/.*$))/m';
+
+		return preg_replace_callback($regex, function ($matches) {
+			$match = $matches[0];
+
+			if (str_contains($match, '/extensions-ucp/mw139/')) {
+				return $match;
+			} elseif (str_contains($match, '/extensions-ucp/v2/mw139/')) {
+				return str_replace('/extensions-ucp/v2/mw139/', '/extensions-ucp/mw139/', $match);
+			} elseif (str_contains($match, '/extensions-ucp/v2/')) {
+				return str_replace('/extensions-ucp/v2/', '/extensions-ucp/mw139/', $match);
+			} else {
+				return str_replace('/extensions-ucp/', '/extensions-ucp/mw139/', $match);
+			}
+		}, $userNote);
 	}
 }
