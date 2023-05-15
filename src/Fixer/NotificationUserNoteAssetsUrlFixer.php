f251aa688652f2deb6d8a61c9f9169e56a4be037
Paweł Świszcz <pswiszcz@fandom.com>
2023-05-15 13:26:24 +0200

    UXFCP-3645 (fix): Removed wrong assets url from notification user_note
    The assets should not have /extensions-ucp/v2 in url. It should be replaced with /extensions-ucp/mw139/ (code style fix)

--- src/Fixer/NotificationUserNoteAssetsUrlFixer.php
+++ src/Fixer/NotificationUserNoteAssetsUrlFixer.php
@@ -5,6 +5,12 @@ declare( strict_types=1 );
 namespace Reverb\Fixer;
 
 final class NotificationUserNoteAssetsUrlFixer {
+	private const REGEX =
+		'/(<img\s+[^>]*?\bsrc=["\'].*?)' .
+		'((?!.*\/extensions-ucp\/mw139\/.*).*|' .
+		'^(?=.*\/extensions-ucp\/v2\/.*$).*|' .
+		'^(?=.*\/extensions-ucp\/[^\/]+\/.*$))/m';
+
 	public function __construct( private string $domain ) {
 	}
 
@@ -13,7 +19,7 @@ final class NotificationUserNoteAssetsUrlFixer {
 			return $notification;
 		}
 
-		$notification['user_note'] = $this->replaceNotificationImgAssetUrl($notification['user_note']);
+		$notification['user_note'] = $this->replaceNotificationImgAssetUrl( $notification['user_note'] );
 
 		// replace relative path with full urls, so notifications displayed outside of wiki context
 		// (i.e. fandom.com page) work well
@@ -26,22 +32,19 @@ final class NotificationUserNoteAssetsUrlFixer {
 		return $notification;
 	}
 
-	private function replaceNotificationImgAssetUrl($userNote): array|string|null
-	{
-		$regex = '/(<img\s+[^>]*?\bsrc=["\'].*?)((?!.*\/extensions-ucp\/mw139\/.*).*|^(?=.*\/extensions-ucp\/v2\/.*$).*|^(?=.*\/extensions-ucp\/[^\/]+\/.*$))/m';
-
-		return preg_replace_callback($regex, function ($matches) {
+	private function replaceNotificationImgAssetUrl( $userNote ): array|string|null {
+		return preg_replace_callback( self::REGEX, static function ( $matches ) {
 			$match = $matches[0];
 
-			if (str_contains($match, '/extensions-ucp/mw139/')) {
+			if ( str_contains( $match, '/extensions-ucp/mw139/' ) ) {
 				return $match;
-			} elseif (str_contains($match, '/extensions-ucp/v2/mw139/')) {
-				return str_replace('/extensions-ucp/v2/mw139/', '/extensions-ucp/mw139/', $match);
-			} elseif (str_contains($match, '/extensions-ucp/v2/')) {
-				return str_replace('/extensions-ucp/v2/', '/extensions-ucp/mw139/', $match);
+			} elseif ( str_contains( $match, '/extensions-ucp/v2/mw139/' ) ) {
+				return str_replace( '/extensions-ucp/v2/mw139/', '/extensions-ucp/mw139/', $match );
+			} elseif ( str_contains( $match, '/extensions-ucp/v2/' ) ) {
+				return str_replace( '/extensions-ucp/v2/', '/extensions-ucp/mw139/', $match );
 			} else {
-				return str_replace('/extensions-ucp/', '/extensions-ucp/mw139/', $match);
+				return str_replace( '/extensions-ucp/', '/extensions-ucp/mw139/', $match );
 			}
-		}, $userNote);
+		}, $userNote );
 	}
 }
--- tests/Unit/Fixer/NotificationUserNoteAssetsUrlFixerTest.php
+++ tests/Unit/Fixer/NotificationUserNoteAssetsUrlFixerTest.php
@@ -5,36 +5,34 @@ namespace Tests\Unit\Fixer;
 use PHPUnit\Framework\TestCase;
 use Reverb\Fixer\NotificationUserNoteAssetsUrlFixer;
 
-class NotificationUserNoteAssetsUrlFixerTest extends TestCase
-{
+class NotificationUserNoteAssetsUrlFixerTest extends TestCase {
 	/**
 	 * @dataProvider cheevosDataProvider
 	 * @param string $input
 	 * @param string $expectedResult
 	 * @return void
 	 */
-	public function testCheevosHasCorrectPath(string $input, string $expectedResult): void
-	{
-		$fixer = new NotificationUserNoteAssetsUrlFixer('fandom.com');
+	public function testCheevosHasCorrectPath( string $input, string $expectedResult ): void {
+		$fixer = new NotificationUserNoteAssetsUrlFixer( 'fandom.com' );
 
 		$notificationInput = [
 			'user_note' => $input
 		];
 
-		$fixedNotification = $fixer->fix($notificationInput);
+		$fixedNotification = $fixer->fix( $notificationInput );
 
-		self::assertSame($expectedResult, $fixedNotification['user_note']);
+		self::assertSame( $expectedResult, $fixedNotification['user_note'] );
 	}
 
-	public function cheevosDataProvider(): array
-	{
+	public function cheevosDataProvider(): array {
 		$fullNotification = '
 			<div class=\'reverb-npn-ach\'>
 				<div class=\'reverb-npn-ach-text\'>
 					<div class=\'reverb-npn-ach-name\'>What do we have here?</div>
 					<div class=\'reverb-npn-ach-description\'>Visit the Achievements Special Page.</div>
 				</div>
-				<div class=\'reverb-npn-ach-points\'>10<img src="/extensions-ucp/Cheevos/images/gp30.png" />
+				<div class=\'reverb-npn-ach-points\'>10
+					<img src="/extensions-ucp/Cheevos/images/gp30.png" />
 				</div>
 			</div>
 		';
@@ -45,7 +43,8 @@ class NotificationUserNoteAssetsUrlFixerTest extends TestCase
 					<div class=\'reverb-npn-ach-name\'>What do we have here?</div>
 					<div class=\'reverb-npn-ach-description\'>Visit the Achievements Special Page.</div>
 				</div>
-				<div class=\'reverb-npn-ach-points\'>10<img src="fandom.com/extensions-ucp/mw139/Cheevos/images/gp30.png" />
+				<div class=\'reverb-npn-ach-points\'>10
+					<img src="fandom.com/extensions-ucp/mw139/Cheevos/images/gp30.png" />
 				</div>
 			</div>
 		';
